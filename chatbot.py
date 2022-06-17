184cdc465fef9a5468d985b4c407f560b59dc7ad
CM0isA <costi.moisa98@gmail.com>
2022-06-17 09:56:46 +0300

    trainning data

--- chatbot.py
+++ chatbot.py
@@ -4,21 +4,60 @@ import pickle
 import numpy as np
 
 import nltk
-from nltk.stem import WordNetLemmatizer
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation, Dropout
 from tensorflow.keras.optimizers import SGD
 
-lemmatizer = WordNetLemmatizer
+lemmatizer = nltk.WordNetLemmatizer()
 
-intens = json.load(open('intents.json').read())
+intents = json.loads(open('intents.json').read())
 
 
 words=[]
 classes=[]
+documents=[]
 ignore_letters=['?','.',',','!']
 
-for intent in intens:
-    for pattern in intens["patterns"]:
-        word_list = nltk.tokenize(pattern)
+for intent in intents['intents']:
+    for pattern in intent["patterns"]:
+        # print(pattern)
+        word_list = nltk.word_tokenize(pattern)
+        words.extend(word_list)
+        documents.append((word_list, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+words1 = []
+for word in words: 
+    if word not in ignore_letters:
+        words1.append(lemmatizer.lemmatize(word))
+
+words = sorted(set(words1))
+
+classes = sorted(set(classes))
+
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
+
+
+training = []
+
+output_empty = [0] * len(classes)
+
+for document in documents:
+    bag = []
+    word_patterns = document[0]
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+    training.append([bag, output_row])
+
+random.shuffle(training)
+training = np.array(training)
+
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
